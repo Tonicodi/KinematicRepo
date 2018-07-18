d90ca7ac05b0fb864823bf69a5d5972ab0e4c04d
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2018-07-18 17:16:22 -0600

    cambios en fk

--- Kinematics/src/InverseK.java
+++ Kinematics/src/InverseK.java
@@ -10,23 +10,22 @@ public class InverseK {
     }
 
 
-    public double[] getAngles(float coord[]) {
+    public double[] getAngles(double coord[]) {
 
         double Q[] = new double[3];
 
 
         System.out.println("\nX " + coord[0]+ " Y "+ coord[1] + " Z "+ coord[2]);
 
-        float r =(float) Math.sqrt(Math.pow(coord[0],2)+Math.pow(coord[1],2));
-       // float D = (float) Math.sqrt(Math.pow(coord[3]-Long_arm[0],2) + Math.pow(r,2));
+        double r = Math.sqrt(Math.pow(coord[0],2)+Math.pow(coord[1],2));
 
 
 
-        float gamma = (float) atan2(coord[1], coord[0]);
-        float beta = (float) acos((Math.pow(coord[2]-Long_arm[0],2) + Math.pow(coord[0],2) +  Math.pow(coord[1],2) - Math.pow(Long_arm[2],2) - Math.pow(Long_arm[1],2) )
+        double gamma =  atan2(coord[1], coord[0]);
+        double beta =  acos((Math.pow(coord[2]-Long_arm[0],2) + Math.pow(coord[0],2) +  Math.pow(coord[1],2) - Math.pow(Long_arm[2],2) - Math.pow(Long_arm[1],2) )
                 / 2*Long_arm[1]*Long_arm[2] );
 
-        float alpha = (float) (atan2(r,coord[2]-Long_arm[0]) - atan2(Long_arm[1] + Long_arm[2]* cos(beta),Long_arm[2]*sin(beta)));
+        double alpha =  (atan2(r,coord[2]-Long_arm[0]) - atan2(Long_arm[1] + Long_arm[2]* cos(beta),Long_arm[2]*sin(beta)));
 
 
         Q[0] = gamma;
--- Kinematics/src/Main.java
+++ Kinematics/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main{
 
 
@@ -6,26 +8,21 @@ public class Main{
 
 
         double angulos[];
-        float[] coord_cartesian = new float[3];
-        double L[] = new double[]{50,70,75};
+        double[] coord_cartesian = new double[3];
+        double L[] = new double[]{10,5,5};
         double Q[] = new double[]{0,0,0};
         double cartesiano[];
         ForwardK fk = new ForwardK(L);
         InverseK ik = new InverseK(L);
 
         coord_cartesian[0] = 0;
-        coord_cartesian[1] = 50;
-        coord_cartesian[2] = 0;
+        coord_cartesian[1] = 10;
+        coord_cartesian[2] = 10;
         angulos = ik.getAngles(coord_cartesian);
 
-        angulos[0] = angulos[0] * 180/Math.PI;
-        angulos[1] = angulos[1] * 180/Math.PI;
-        angulos[2] = angulos[2] * 180/Math.PI;
+        cartesiano   = fk.getCartesian(new double[]{0,0,0});
 
-        cartesiano   = fk.getCartesian(new double[]{angulos[0],angulos[1],angulos[2]});
-
-        System.out.println("fk  Px  "+cartesiano[1] + " Py " +cartesiano[2] + " Pz "+cartesiano[0] );
-
+        System.out.println("fk  Px  "+ Arrays.toString(cartesiano));
 
         System.out.println("ik  Q[0]  "+angulos[0] + " Q[1] " +angulos[1] + " Q[2] "+angulos[2] );
 
