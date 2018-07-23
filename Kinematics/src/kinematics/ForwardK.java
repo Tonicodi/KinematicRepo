d505b840494a2f399385a025c02e930249e182bc
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2018-07-23 07:44:37 -0600

    agregado manifest y el package kinematics

---
+++ Kinematics/src/kinematics/ForwardK.java
@@ -0,0 +1,60 @@
+package kinematics;
+
+import static java.lang.Math.*;
+
+
+/**
+ *
+ *            +Z  -Y
+ *             |  /
+ *             | /
+ *             |/
+ *    -X ------/------- +X
+ *            /|
+ *           / |
+ *          /  |
+ *        +Y  -Z
+ *
+ * Orientacion
+ *
+ * Q[0]   -> base
+ * Q[1]   -> antebrazo
+ * Q[2]   -> brazo
+ *
+ */
+
+public class ForwardK {
+    private double Q[];
+    private double L[];
+
+    public ForwardK(double[] l) {
+        L = l;
+    }
+
+    /**
+     * Recibe de entrada un vector q de 3 posiciones con angulos en radianes
+     * lo convierte valores x,y,z usando formulas de cinematica directa
+     *
+     * @param q
+     * @return
+     */
+    public double[] getCartesian(double[] q) {
+        Q = q;
+
+        if (q.length < 3) return null;
+
+        double x = ( L[2] * cos(Q[1] + Q[2]) + L[1] * cos(Q[1]) ) * cos(Q[0]);//ok
+
+
+        double y = ( L[2] * cos(Q[1] + Q[2]) + L[1] * cos(Q[1]) ) * sin(Q[0]);//ok
+
+        double z =   L[2] * sin(Q[1] + Q[2]) + L[1] * sin(Q[1])  + L[0];//ok
+
+        double cartesian[] = new double[3];
+        cartesian[0] = x;
+        cartesian[1] = y;
+        cartesian[2] = z;
+
+        return cartesian;
+    }
+}
---
+++ Kinematics/src/kinematics/InverseK.java
@@ -0,0 +1,39 @@
+package kinematics;
+
+import static java.lang.Math.*;
+public class InverseK {
+
+    private double L[];
+
+
+    public InverseK(double[] long_arm) {
+        this.L = long_arm;
+    }
+
+
+    public double[] getAngles(double coord[]) {
+        double x= coord[0];
+        double y= coord[1];
+        double z= coord[2];
+
+
+        System.out.println("\nX " +x+ " Y "+ y + " Z "+ z);
+
+
+        double gamma =  atan2(y,x);
+        double c3 = (x*x + y*y + z*z  - ( L[0]*L[0] + L[1]*L[1] + L[2]*L[2] ) - 2*L[0]*(z-L[0]))/(2*L[1]*L[2]);
+        double s3 = Math.sqrt(1-c3);
+        double beta = atan2(s3,c3);
+        double alpha =  atan2(( (z-L[0])*(cos(gamma) - sin(gamma)) ),(x-y)) - atan2((s3*L[2]),(c3*L[2]+L[1]));
+
+        double Q[] = new double[3];
+        Q[0] = gamma;
+        Q[1] = alpha;
+        Q[2] = beta;
+
+        /** Salidas en de angulos en radianes **/
+        //System.out.println("Q[0]  "+Q[0] + " Q[1] " +Q[1] + " Q[2] "+Q[2] );
+
+        return Q;
+    }
+}
